224a0b9125bfd5798fbaa54b6c7c2ae25c648c67
Michael Nouzieres <michael.nouzieres@gmail.com>
2026-07-31 14:07:23 -0400

    adding counter logic

--- frontend/src/pages/Stats.jsx
+++ frontend/src/pages/Stats.jsx
@@ -1,3 +1,28 @@
+import { useEffect, useState } from "react";
+
 export default function Stats() {
-  return <></>;
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    incrementVisitorCount();
+  }, []);
+
+  async function incrementVisitorCount() {
+    const response = await fetch(
+      `${import.meta.env.VITE_API_URL}/visitor-count`,
+      {
+        method: "POST",
+      },
+    );
+
+    const data = await response.json();
+    setCount(data.counter);
+  }
+
+  return (
+    <section className="max-w-5xl mx-auto py-5  grid gap-6 lg:grid-cols-2 lg:items-center text-white text-center lg:text-justify">
+      <h1 className="text-4xl font-bold">Total visits to my website!</h1>
+      <p className="text-lg">{count}</p>
+    </section>
+  );
 }
