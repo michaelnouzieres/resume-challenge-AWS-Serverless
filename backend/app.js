446b471587b415201aa4d98a38312da050b9148a
Michael Nouzieres <michael.nouzieres@gmail.com>
2026-08-03 18:39:27 -0400

    fixes lambda code

--- backend/app.js
+++ backend/app.js
@@ -4,9 +4,20 @@ const cors = require("cors");
 const visitorController = require("./controllers/visitorController.js");
 
 export const handler = async (event) => {
+  const corsHeaders = {
+    "Access-Control-Allow-Origin": "https://michaelnouzieres.com",
+    "Access-Control-Allow-Methods": "POST, OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type",
+  };
   try {
     const response = await visitorController.addCount();
-    return response;
+    return {
+      ...response,
+      headers: {
+        ...corsHeaders,
+        ...(response.headers || {}),
+      },
+    };
   } catch (error) {
     return {
       statusCode: 500,
